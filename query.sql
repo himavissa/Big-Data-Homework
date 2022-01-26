187416db509d55ddeb667684a6e167cbf42961fb
himavissa <84814543+himavissa@users.noreply.github.com>
2022-01-26 06:02:05 -0800

    Add files via upload

---
+++ query.sql
@@ -0,0 +1,14 @@
+SELECT * FROM products
+LIMIT 10;
+
+SELECT COUNT(*) FROM products;
+
+SELECT * FROM customers
+LIMIT 10
+
+SELECT COUNT(*) FROM customers;
+
+SELECT * FROM vine_table
+LIMIT 10;
+
+SELECT COUNT(*) FROM vine_table;
---
+++ toys.sql
@@ -0,0 +1,11 @@
+SELECT * FROM products
+LIMIT 10;
+
+SELECT COUNT(*) FROM review_id_table;
+
+SELECT COUNT(*) FROM products;
+
+SELECT * FROM vine_table
+LIMIT 10;
+
+SELECT COUNT(*) FROM vine_table;
